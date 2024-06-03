34a05aa97e61ff0c2be3e0870e7da1ced0790610
Keernom <keernom.gdev@gmail.com>
2024-06-03 23:25:09 +0300

    feat: circle unit tests added

---
+++ ShapesAreaCalcSolution/ShapesAreaCalcTests/ShapeTests/CircleTests.cs
@@ -0,0 +1,61 @@
+﻿using ShapesAreaCalc.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAreaCalcTests.ShapeTests
+{
+    public class CircleTests
+    {
+        private bool IsAreaCorrect(double exp, double actual)
+        {
+            return Math.Abs(exp - actual) < 0.1;
+        }
+
+        [Fact]
+        public void IntRaduisInput()
+        {
+            var circle = new Circle(5);
+
+            double expectedValue = 78.5;
+            double circleArea = circle.GetShapeArea();
+
+            Assert.True(IsAreaCorrect(expectedValue, circleArea));
+        }
+
+        [Fact]
+        public void DoubleRaduisInput()
+        {
+            var circle = new Circle(5.5);
+
+            double expectedValue = 94.98;
+            double circleArea = circle.GetShapeArea();
+
+            Assert.True(IsAreaCorrect(expectedValue, circleArea));
+        }
+
+        [Fact]
+        public void NegativeRadiusInput()
+        {
+            var circle = new Circle(-5.5);
+
+            double expectedValue = 94.98;
+            double circleArea = circle.GetShapeArea();
+
+            Assert.True(IsAreaCorrect(expectedValue, circleArea));
+        }
+
+        [Fact]
+        public void ZeroRadiusInput()
+        {
+            var circle = new Circle(0);
+
+            double expectedValue = 0;
+            double circleArea = circle.GetShapeArea();
+
+            Assert.True(IsAreaCorrect(expectedValue, circleArea));
+        }
+    }
+}
