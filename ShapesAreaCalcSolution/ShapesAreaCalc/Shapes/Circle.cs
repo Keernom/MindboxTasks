fdbe30398c9af010250d319c0a973f5f2bf0a3b2
Keernom <keernom.gdev@gmail.com>
2024-06-03 23:24:48 +0300

    feat: circle shape added

---
+++ ShapesAreaCalcSolution/ShapesAreaCalc/Shapes/Circle.cs
@@ -0,0 +1,22 @@
+﻿using ShapesAreaCalc.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAreaCalc.Shapes
+{
+    public class Circle : BaseShape, IShapable
+    {
+        public Circle(double radius)
+        {
+            SetShapeParams(radius);
+        }
+
+        public double GetShapeArea()
+        {
+            return Math.PI * Math.Pow(ShapeParams.First(), 2);
+        }
+    }
+}
