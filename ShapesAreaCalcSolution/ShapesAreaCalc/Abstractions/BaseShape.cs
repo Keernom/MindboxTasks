4ec185c3a00b58e976020814b2dfd80a71758e47
Keernom <keernom.gdev@gmail.com>
2024-06-03 23:24:27 +0300

    feat: base abstractions added

---
+++ ShapesAreaCalcSolution/ShapesAreaCalc/Abstractions/BaseShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAreaCalc.Abstractions
+{
+    public abstract class BaseShape
+    {
+        protected List<double> ShapeParams { get; private set; } = new List<double>();
+
+        public void SetShapeParams(params double[] shapeParams)
+        {
+            ShapeParams.AddRange(shapeParams);
+        }
+    }
+}
---
+++ ShapesAreaCalcSolution/ShapesAreaCalc/Abstractions/IShapable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAreaCalc.Abstractions
+{
+    internal interface IShapable
+    {
+        public double GetShapeArea();
+    }
+}
