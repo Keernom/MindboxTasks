b6c22d4b0aea2fbb59c24e473bcf7d873bdec929
Keernom <keernom.gdev@gmail.com>
2024-06-05 17:20:30 +0300

    faet: base shape class deleted and circle ctor rewrited

--- ShapesAreaCalcSolution/ShapesAreaCalc/Shapes/Circle.cs
+++ ShapesAreaCalcSolution/ShapesAreaCalc/Shapes/Circle.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 
 namespace ShapesAreaCalc.Shapes
 {
-    public class Circle : BaseShape, IShapable
+    public class Circle : IShapable
     {
-        public Circle(double radius) : base(radius) { }
+        double _radius;
+
+        public Circle(double radius) 
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("The circle radius can't be less than zero");
+            }
+
+            _radius = radius;
+        }
 
         public double GetShapeArea()
         {
-            return Math.PI * Math.Pow(ShapeParams.First(), 2);
+            return Math.PI * Math.Pow(_radius, 2);
         }
 
         public double GetShapeArea()
