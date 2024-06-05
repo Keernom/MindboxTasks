82ff3e8b6a4b79f79f281cffac66c5a2d23c8a0a
Keernom <keernom.gdev@gmail.com>
2024-06-05 17:21:15 +0300

    feat: baseShapeTests class added

---
+++ ShapesAreaCalcSolution/ShapesAreaCalcTests/ShapeTests/BaseShapeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAreaCalcTests.ShapeTests
+{
+    public abstract class BaseShapeTests
+    {
+        protected bool IsAreaCorrect(double exp, double actual)
+        {
+            return Math.Abs(exp - actual) < 0.1;
+        }
+    }
+}
